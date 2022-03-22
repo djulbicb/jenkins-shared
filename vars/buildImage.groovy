62f89d5ef571c3edaa8e78a6a2883a478ce279f3
djulbicb <djulbic.bojan@gmail.com>
2022-03-22 12:18:24 +0100

    Add commands as class

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/env.groovy
 import com.example.Docker
 def call(String credentialsId, String imageName) {
-    return new Docker(this).login(credentialsId, imageName)
+    def docker = new Docker(this)
+    docker.login(credentialsId)
+    docker.buildImage(imageName)
+    docker.puchImage(imageName)
 }
