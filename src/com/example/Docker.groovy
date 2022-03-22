6abbc5bd30134d6311557f1cf1820036df487487
djulbicb <djulbic.bojan@gmail.com>
2022-03-22 12:02:42 +0100

    Add commands as class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,13 +9,17 @@ class Docker {
         this.script = script
     }
 
-    def buildDockerImage(String imageName) {
+    def login(String credentialsId) {
+        script.echo 'Login to docker...'
+        script.sh "echo $script.PASS | docker login -u $script.USER --pasword-stdin"
+    }
+
+    def buildImage(String imageName) {
         script.echo 'building the docker image...'
-        script.withCredentials([script.usernamePassword(creentialsId: 'docker-hub-repo', passwordVariable: "PASS", usernameVariable: "User")]) {
-            script.sh "docker build -t $imageName ."
-            script.sh "echo $script.PASS | docker login -u $script.USER --pasword-stdin"
-            script.sh "echo $script.PASS | docker login - $script.USER --pasword-stdin"
-            script.sh "docker push $imageName"
-        }
+        script.sh "docker build -t $imageName ."
+    }
+
+    def puchImage(String imageName) {
+        script.sh "docker push $imageName"
     }
 }
